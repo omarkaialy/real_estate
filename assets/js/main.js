28d27c2d12e1a41c79ec5b4e19e0a38d5110ffb0
omarlord1221 <omar12kaialy@gmail.com>
2022-11-07 10:59:21 +0200

    start the project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,31 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== SWIPER POPULAR ===============*/
 
-
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll(".value__accordion-item");
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".value__accordion-header");
 
+  accordionHeader.addEventListener("click", () => {
+    toggleItem(item);
+  });
+});
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value__accordion-content");
+  if (accordionContent.classList.contains("accordion-open")) {
+    item.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
