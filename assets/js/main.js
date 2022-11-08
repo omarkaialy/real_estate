8bb24d3dff630b8c84ac0ba974301f2078fdf96b
omarlord1221 <omar12kaialy@gmail.com>
2022-11-08 02:23:57 +0200

    add home

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,17 @@ accordionItems.forEach((item) => {
   const accordionHeader = item.querySelector(".value__accordion-header");
 
   accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
     toggleItem(item);
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
   });
 });
 const toggleItem = (item) => {
   const accordionContent = item.querySelector(".value__accordion-content");
-  if (accordionContent.classList.contains("accordion-open")) {
-    item.removeAttribute("style");
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
     item.classList.remove("accordion-open");
   } else {
     accordionContent.style.height = accordionContent.scrollHeight + "px";
