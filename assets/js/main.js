bd43be9479eee668cd5c0f14492bf1980a2ed754
omarlord1221 <omar12kaialy@gmail.com>
2022-12-06 23:53:58 +0200

    add javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,3 +112,159 @@ sr.reveal(".value__content", { origin: "right" });
 
 sr.reveal(".contact__images", { origin: "right" });
 sr.reveal(".contact__content", { origin: "left" });
+// Api Request
+
+function create() {
+  var Appartment = Parse.Object.extend("Appartment");
+  const mypet = new Appartment();
+  var modal = document.getElementById("modal__add");
+
+  var textName = "myName";
+  mypet.set("name", textName);
+
+  mypet
+    .save()
+    .then(function (appartment) {
+      console.log(
+        "Appartment created successful with name: " +
+          appartment.get("name") +
+          " and age: " +
+          appartment.get("agePet")
+      );
+      modal.style.display = "none";
+    })
+    .catch(function (error) {
+      console.log("Error: " + error.message);
+    });
+}
+// Api Read
+async function read() {
+  var Appartment = Parse.Object.extend("Appartment");
+  query = new Parse.Query(Appartment);
+
+  let result = await query.find();
+  for (let index = 0; index < result.length; index++) {
+    const element = result[index];
+    console.log(
+      "House Name " +
+        element.get("name") +
+        " House Price " +
+        element.get("price")
+    );
+  }
+  // query
+  //   .first()
+  //   .then(function (pet) {
+  //     if (pet) {
+  //       console.log(
+  //         "Appartment found successful with name: " +
+  //           pet.get("name") +
+  //           " and address: " +
+  //           pet.get("address")
+  //       );
+  //     } else {
+  //       console.log("Nothing found, please try again");
+  //     }
+  //   })
+  //   .catch(function (error) {
+  //     console.log("Error: " + error.code + " " + error.message);
+  //   });
+}
+// Api Update
+
+function readThenUpdate() {
+  var Appartment = Parse.Object.extend("Appartment");
+  textName = "myName";
+
+  query = new Parse.Query(Appartment);
+  query.equalTo("name", textName);
+  query
+    .first()
+    .then(function (pet) {
+      if (pet) {
+        console.log("Appartment found with name: " + pet.get("name"));
+        update(pet);
+      } else {
+        console.log("Nothing found, please try again");
+      }
+    })
+    .catch(function (error) {
+      console.log("Error: " + error.code + " " + error.message);
+    });
+}
+function update(foundPet) {
+  textName = "myName";
+  foundPet.set("name", textName);
+
+  foundPet
+    .save()
+    .then(function (pet) {
+      console.log("Appartment updated! Name: " + pet.get("name"));
+    })
+    .catch(function (error) {
+      console.log("Error: " + error.message);
+    });
+}
+// Api Delete
+
+function readThenDelete() {
+  var Appartment = Parse.Object.extend("Appartment");
+  textName = "myName";
+  query = new Parse.Query(Appartment);
+  query.equalTo("name", textName);
+  query
+    .first()
+    .then(function (pet) {
+      if (pet) {
+        console.log("Appartment found with name: " + pet.get("name"));
+        deletePet(pet);
+      } else {
+        console.log("Nothing found, please try again");
+        return null;
+      }
+    })
+    .catch(function (error) {
+      console.log("Error: " + error.code + " " + error.message);
+      return null;
+    });
+}
+
+function deletePet(foundPet) {
+  foundPet
+    .destroy()
+    .then(function (response) {
+      console.log(
+        "Appartment " + foundPet.get("name") + " erased successfully"
+      );
+    })
+    .catch(function (response, error) {
+      console.log("Error: " + error.message);
+    });
+}
+const showModalAdd = document.querySelectorAll(".subscribe__button");
+showModalAdd.forEach((item) => {
+  item.addEventListener("click", () => {
+    showAddModal();
+  });
+});
+//  Modal
+function showAddModal() {
+  var modal = document.getElementById("modal__add");
+  modal.style.display = "block";
+  // Get the <span> element that closes the modal
+  var span = document.getElementById("close_modal");
+  var createbtn = document.getElementById("createbtn");
+  span.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+  createbtn.addEventListener("click", () => {
+    create();
+  });
+}
+
+window.onclick = function (event) {
+  var modal = document.getElementById("modal__add");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
